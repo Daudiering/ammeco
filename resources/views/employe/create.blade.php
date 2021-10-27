f05de6ee4bc7898411eb027d8e0f3d04039472e9
Ricardolito <Artengineer@Transversal.ht>
2021-10-27 07:26:14 -0400

    first commit

---
+++ resources/views/employe/create.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('Creation Employees') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <form method="POST" action="{{ route('employe.store') }}">
+                        <div class="form-group">
+                            <label for="name">Fullname</label>
+                            <input type="text" name="name" class="form-control" id="name"  placeholder="Enter your Fullname" >
+                            <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="email"  placeholder="Enter email" name="email">
+                            <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
+            
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/employe/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('Employees List') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <a class="btn btn-primary mb-2" href="{{ route('employe.create') }}">Add an Employee</a>
+                  
+                  <table class="table">
+                    <thead class="thead-light">
+                      <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Full Name</th>
+                        <th scope="col">Email</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                       {{-- <th scope="row">3</th> --}}
+                        @foreach ($employes as $employe)
+                            <td>{{ $employe->id }}</td>
+                            <td>{{ $employe->name }}</td>
+                            <td>{{ $employe->email }}</td>
+                        @endforeach
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
